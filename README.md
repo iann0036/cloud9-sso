708f45e562834e6028bf997fec83b10c7d50374b
Ian Mckay <ian.mckay@kablamo.com.au>
2020-12-23 12:09:41 +1100

    Initial work

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# cloud9-sso
-Add Cloud9 environments to AWS SSO
+# AWS Cloud9 SSO Integration
+
+<img src="https://github.com/iann0036/cloud9-sso/raw/master/assets/screen1.png" width="245" height="145">
+
+:exclamation: **CAUTION:** This project is currently in beta stages. Some resources may not work as expected. Please [report these](https://github.com/iann0036/cloud9-sso/issues) if you find them.
+
+
+## Installation
+
+### Execution Infrastructure
+
+[![Launch Stack](https://cdn.rawgit.com/buildkite/cloudformation-launch-stack-button-svg/master/launch-stack.svg)](https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/new?stackName=myc9env&templateURL=https://s3.amazonaws.com/ianmckay-us-east-1/cloud9-sso/template.yaml)
+
+Click the above link to deploy the stack to your environment. This stack creates a Cloud9 environment and the SAML handling API Gateway / Lambda combination.
+
+If you prefer, you can also manually upsert the [template.yaml](https://github.com/iann0036/cloud9-sso/blob/master/template.yaml) stack from source.
+
+### SSO Setup
+
+_TBC_
+
+![](assets/screen2.png)
+
+![](assets/screen3.png)
+
+![](assets/screen4.png)
---
+++ lambda/index.js
@@ -0,0 +1,176 @@
+const AWS = require('aws-sdk');
+var winston = require('winston');
+var saml2 = require('saml2-js');
+var axios = require('axios').default;
+
+var LOG = winston.createLogger({
+    level: process.env.LOG_LEVEL.toLowerCase(),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+var ssm = new AWS.SSM();
+var sts = new AWS.STS();
+var cloud9 = new AWS.Cloud9();
+
+async function decodeSAMLResponse(sp, idp, samlresponse) {
+    let resp = await new Promise((resolve,reject) => {
+        sp.post_assert(idp, {
+            request_body: {
+                'SAMLResponse': samlresponse
+            }
+        }, function(err, resp) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(resp);
+            }
+        });
+    });
+    
+    return resp;
+}
+
+function decodeForm(form) {
+    var ret = {};
+
+    var items = form.split("&");
+    items.forEach(item => {
+        var split = item.split("=");
+        ret[split.shift()] = split.join("=");
+    });
+
+    return ret;
+}
+
+async function getUserBySAML(samlresponse) {
+    let ssoparamresponse = await ssm.getParameter({
+        Name: process.env.SSO_SSM_PARAMETER
+    }).promise();
+
+    LOG.debug("Param is:");
+    LOG.debug(ssoparamresponse['Parameter']['Value']);
+
+    let ssoproperties = JSON.parse(ssoparamresponse['Parameter']['Value']);
+    
+    var sp_options = {
+        entity_id: "https://" + process.env.AWS_LAMBDA_FUNCTION_NAME.replace("-SSOServicer","") + ".local/metadata.xml",
+        private_key: "",
+        certificate: "",
+        assert_endpoint: "",
+        allow_unencrypted_assertion: true
+    };
+    var sp = new saml2.ServiceProvider(sp_options);
+
+    LOG.debug("sp is:");
+    LOG.debug(sp);
+    
+    var idp_options = {
+        sso_login_url: ssoproperties['SignInURL'],
+        sso_logout_url: ssoproperties['SignOutURL'],
+        certificates: [ssoproperties['Certificate']],
+        allow_unencrypted_assertion: true
+    };
+    var idp = new saml2.IdentityProvider(idp_options);
+
+    let samlattrs = await decodeSAMLResponse(sp, idp, decodeURIComponent(samlresponse));
+
+    LOG.debug("samlattrs is:");
+    LOG.debug(samlattrs);
+
+    return {
+        'guid': samlattrs['user']['attributes']['guid'][0],
+        'email': samlattrs['user']['attributes']['email'][0],
+        'samlresponse': decodeURIComponent(samlresponse),
+        'ssoprops': ssoproperties
+    };
+}
+
+async function handleSAMLResponse(event) {
+    let body = event.body;
+    if (event.isBase64Encoded) {
+        body = Buffer.from(event.body, 'base64').toString('utf8');
+    }
+
+    LOG.debug("Body is:");
+    LOG.debug(body);
+
+    var form = decodeForm(body);
+
+    LOG.debug("Form is:");
+    LOG.debug(form);
+
+    let user = await getUserBySAML(form['SAMLResponse']);
+
+    LOG.debug("User is:");
+    LOG.debug(user);
+
+    let ssoroleassume = await sts.assumeRole({
+        RoleArn: process.env.SSO_ROLE,
+        RoleSessionName: user.guid
+    }).promise();
+
+    LOG.debug('https://signin.aws.amazon.com/federation?Action=getSigninToken&Session=' + encodeURIComponent(JSON.stringify({
+		'sessionId': ssoroleassume.Credentials.AccessKeyId,
+		'sessionKey': ssoroleassume.Credentials.SecretAccessKey,
+		'sessionToken': ssoroleassume.Credentials.SessionToken,
+	})));
+
+    let signintoken = await axios.get('https://signin.aws.amazon.com/federation?Action=getSigninToken&Session=' + encodeURIComponent(JSON.stringify({
+		'sessionId': ssoroleassume.Credentials.AccessKeyId,
+		'sessionKey': ssoroleassume.Credentials.SecretAccessKey,
+		'sessionToken': ssoroleassume.Credentials.SessionToken,
+    })));
+    
+    let c9membership = await cloud9.createEnvironmentMembership({
+        environmentId: process.env.ENVIRONMENT_ID,
+        permissions: "read-write",
+        userArn: "arn:aws:sts::" + process.env.ACCOUNT_ID + ":assumed-role/" + process.env.AWS_LAMBDA_FUNCTION_NAME.replace("-SSOServicer","") + "-SSOUser/" + user.guid
+    }).promise().catch(() => { });
+
+    return {
+        "statusCode": 302,
+        "headers": {
+            "Location": 'https://signin.aws.amazon.com/federation?Action=login&Destination=' + encodeURIComponent('https://console.aws.amazon.com/cloud9/ide/' + process.env.ENVIRONMENT_ID) + '&SigninToken=' + signintoken.data['SigninToken']
+        }
+    };
+}
+
+exports.handler = async (event, context) => {
+    LOG.debug(event);
+
+    if (event.routeKey == "GET /") {
+        let ssoparamresponse = await ssm.getParameter({
+            Name: process.env.SSO_SSM_PARAMETER
+        }).promise();
+
+        let ssoproperties = JSON.parse(ssoparamresponse['Parameter']['Value']);
+        
+        return {
+            "statusCode": 302,
+            "headers": {
+                "Location": ssoproperties['SignOutURL']
+            }
+        };
+    } else if (event.routeKey == "POST /") {
+        try {
+            let resp = await handleSAMLResponse(event);
+
+            return resp;
+        } catch(err) {
+            LOG.error(err);
+        }
+
+        return {
+            "statusCode": 500,
+            "isBase64Encoded": false,
+            "headers": {
+                "Content-Type": "index/html"
+            },
+            "body": ""
+        };
+    } else {
+        return context.succeed();
+    }
+};
